0b94232b2aaa1f8dfb50fa3d2a4c3dcd46a83f46
Om <omnagvekar29@gmail.com>
2023-05-20 21:00:27 +0530

    Auto Delete Funationality fully settings needs to be saved

---
+++ app/src/main/java/com/example/spamsnap/DeleteImagesService.java
@@ -0,0 +1,73 @@
+package com.example.spamsnap;
+
+import static com.example.spamsnap.MainActivity.classifiedImages;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class DeleteImagesService extends Service {
+    public DeleteImagesService() {
+        // Default constructor
+    }
+    private static final String TAG = "DeleteImagesService";
+    private static final long INTERVAL = 24 * 60 * 60 * 1000; // 24 hours
+    public  long CUTOFF = INTERVAL * SettingActivity.day; // 7 days
+
+    Handler mHandler = new Handler();
+
+    Runnable mRunnable = new Runnable() {
+        @Override
+        public void run() {
+            deleteOldImages();
+            mHandler.postDelayed(this, INTERVAL);
+        }
+    };
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+//            long interval = intent.getLongExtra("interval",INTERVAL);
+//            if (intent != null && intent.hasExtra("day")) {
+//                day = intent.getIntExtra("day", 1);
+//            }
+//            CUTOFF = INTERVAL*day;
+        Log.d(TAG, "onStartCommand: "+SettingActivity.day);
+        mHandler.postDelayed(mRunnable, INTERVAL);
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mHandler.removeCallbacks(mRunnable);
+    }
+
+    private void deleteOldImages() {
+        File dir;
+        Log.d("Service", "deleteOldImages: "+SettingActivity.day);
+        ArrayList<Image> ImageToRemove = new ArrayList<>();
+        for (Image image:classifiedImages) {
+            dir = new File(image.imagepath);
+            long cutoff = System.currentTimeMillis() - CUTOFF;
+            if (dir.lastModified() < cutoff) {
+                boolean delete=dir.delete();
+                Log.d("Delete","Deleted Image is:"+image.imagename+"and "+delete);
+                ImageToRemove.add((image));
+            }
+        }
+        classifiedImages.removeAll(ImageToRemove);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
--- app/src/main/java/com/example/spamsnap/SettingActivity.java
+++ app/src/main/java/com/example/spamsnap/SettingActivity.java
@@ -15,8 +15,11 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.Switch;
 
 import java.io.File;
@@ -45,41 +48,66 @@ public class SettingActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         aSwitch = findViewById(R.id.switch1);
-        cardView = findViewById(R.id.cardView);
-        sevenday = findViewById(R.id.sevenday);
-        thirtyday = findViewById(R.id.thirtyday);
-        cardView.setVisibility(View.GONE);
+        Spinner spinner = findViewById(R.id.spinner);
+        String [] array = {"7 Days","30 Days", "Default(24Hrs)"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,R.layout.spinner_item,array);
+
+        adapter.setDropDownViewResource(R.layout.spinner_item);
+        spinner.setAdapter(adapter);
+        spinner.setVisibility(View.INVISIBLE);
+//        cardView = findViewById(R.id.cardView);
+//        sevenday = findViewById(R.id.sevenday);
+//        thirtyday = findViewById(R.id.thirtyday);
+//        cardView.setVisibility(View.GONE);
         aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked){
-                    cardView.setVisibility(View.VISIBLE);
-//                    Intent intent = new Intent(SettingActivity.this, DeleteImagesService.class);
-//                    long interval = 24 * 60 * 60 * 1000; // Default interval is 24 hours
+                    spinner.setVisibility(View.VISIBLE);
+                    spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            String selectedOptions = parent.getItemAtPosition(position).toString();
+                            if (selectedOptions.equals(array[0])){
+                                day=7;
+                                startDeleteImagesService();
+                                DeleteImagesService deleteImagesService =  new DeleteImagesService();
+                                deleteImagesService.mRunnable.run();
+                            } else if (selectedOptions.equals(array[1])) {
+                                day=30;
+                                startDeleteImagesService();
+                                DeleteImagesService deleteImagesService =  new DeleteImagesService();
+                                deleteImagesService.mRunnable.run();
+                            }else if (selectedOptions.equals(array[2])){
+                                day=1;
+                                startDeleteImagesService();
+                                DeleteImagesService deleteImagesService =  new DeleteImagesService();
+                                deleteImagesService.mRunnable.run();
+                            }
+                            else {
+                                day=1;
+                                startDeleteImagesService();
+                                DeleteImagesService deleteImagesService =  new DeleteImagesService();
+                                deleteImagesService.mRunnable.run();
+                            }
+                        }
 
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
 
+                        }
+                    });
 
-                    if ((sevenday.isSelected())){
-//                        interval = 7 * 24 * 60 * 60 * 1000; // 7 days;
-                        day=7;
-                        Log.d("Service", "onCheckedChanged: "+day);
-                    } else if ((thirtyday.isSelected())) {
-//                        interval = 30L * 24 * 60 * 60 * 1000; // 30 days
-                        day=30;
-
-                    }
-                    startDeleteImagesService();
-                    DeleteImagesService deleteImagesService =  new DeleteImagesService();
-                    deleteImagesService.mRunnable.run();
 //                    intent.putExtra("interval",interval);
 //                    startService(intent);
                 }
                 else {
-                    cardView.setVisibility(View.GONE);
+//                    cardView.setVisibility(View.GONE);
                     // Stop the DeleteImagesService and remove the callback
                     stopService(new Intent(getApplicationContext(), DeleteImagesService.class));
                     DeleteImagesService deleteImagesService = new DeleteImagesService();
                     deleteImagesService.mHandler.removeCallbacks(deleteImagesService.mRunnable);
+                    spinner.setVisibility(View.INVISIBLE);
                 }
             }
         });
@@ -93,61 +121,5 @@ public class SettingActivity extends AppCompatActivity {
         intent.putExtra("day", day);
         startService(intent);
     }
-    public class DeleteImagesService extends Service {
-        public DeleteImagesService() {
-            // Default constructor
-        }
-        private static final String TAG = "DeleteImagesService";
-        private static final long INTERVAL = 24 * 60 * 60 * 1000; // 24 hours
-        public  long CUTOFF = INTERVAL * day; // 7 days
 
-        private Handler mHandler = new Handler();
-
-        private Runnable mRunnable = new Runnable() {
-            @Override
-            public void run() {
-                deleteOldImages();
-                mHandler.postDelayed(this, INTERVAL);
-            }
-        };
-
-        @Override
-        public int onStartCommand(Intent intent, int flags, int startId) {
-//            long interval = intent.getLongExtra("interval",INTERVAL);
-            if (intent != null && intent.hasExtra("day")) {
-                day = intent.getIntExtra("day", 1);
-            }
-            CUTOFF = INTERVAL*day;
-            mHandler.postDelayed(mRunnable, INTERVAL);
-            return START_STICKY;
-        }
-
-        @Override
-        public void onDestroy() {
-            super.onDestroy();
-            mHandler.removeCallbacks(mRunnable);
-        }
-
-        private void deleteOldImages() {
-            File dir;
-            Log.d("Service", "deleteOldImages: "+day);
-            ArrayList<Image> ImageToRemove = new ArrayList<>();
-            for (Image image:classifiedImages) {
-                dir = new File(image.imagepath);
-                long cutoff = System.currentTimeMillis() - CUTOFF;
-                if (dir.lastModified() < cutoff) {
-                    boolean delete=dir.delete();
-                    Log.d("Delete","Deleted Image is:"+image.imagename+"and "+delete);
-                    ImageToRemove.add((image));
-                }
-            }
-            classifiedImages.removeAll(ImageToRemove);
-        }
-
-        @Nullable
-        @Override
-        public IBinder onBind(Intent intent) {
-            return null;
-        }
-    }
 }
